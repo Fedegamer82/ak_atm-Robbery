517c931436d6df1e6210fb99da84ed373df13371
Fedegamer82 <103957127+Fedegamer82@users.noreply.github.com>
2023-04-15 20:21:39 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,5 @@
 ESX = nil
-local ox_target = exports.ox_target
-local models = { `prop_atm_01`, `prop_atm_02` }
+local qtarget = exports.qtarget
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -9,26 +8,21 @@ Citizen.CreateThread(function()
 	end
 end)
 
-
-ox_target:addModel(models, {
-	{
-		name = "RapinaATM",
-		icon = "fa-solid fa-money-bill",
-		label = 'Rapina ATM',
-		onSelect = function()
-			ESX.TriggerServerCallback('ak_dev:CheckItem', function(DispositivoHack)
-                if DispositivoHack >= 1 then
-					FreezeEntityPosition(PlayerPedId(), true)
-					TriggerEvent('ak_dev:RapinaAtm')
-				else
-					ESX.ShowNotification("Non hai gli strumenti giusti per rapinare questo ATM")
-				end
-			end)
-		end,
+qtarget:AddTargetModel(Config.PropATM, {
+	options = {
+		{
+			icon = "fa-solid fa-money-bill",
+			label = "Rapina ATM",
+				
+			-- end,
+			event = "ak_illegal:qTarget",
+			num = 1
+		}
 	},
+	distance = 2
 })
 
-RegisterNetEvent('ak_dev:RapinaAtm',function(morte)
+RegisterNetEvent('ak_dev:RapinaAtm',function()
 	exports.rprogress:Custom({
 		Async = false,
 		canCancel = true,
@@ -40,13 +34,23 @@ RegisterNetEvent('ak_dev:RapinaAtm',function(morte)
 			if outcome == 0 then
 				FreezeEntityPosition(PlayerPedId(), false)
 				ESX.ShowNotification('Hai fallito l\'hacking', false, false, 140)
+				ExecuteCommand('e c')
 			elseif outcome == 1 then
-				FreezeEntityPosition(PlayerPedId(), false)
-				TriggerServerEvent('ak_dev:DaiSoldiATM')
+				TriggerEvent("Drilling:Start",function(success)
+					if (success) then
+					--   print("Drilling complete.")
+					FreezeEntityPosition(PlayerPedId(), false)
+					TriggerServerEvent('ak_dev:DaiSoldiATM')
+					ExecuteCommand('e c')
+					else
+					end
+				  end)
 			elseif outcome == 2 then
 				FreezeEntityPosition(PlayerPedId(), false)
 				ESX.ShowNotification('Hai finito il tempo', false, false, 140)
+				ExecuteCommand('e c')
 			elseif outcome == -1 then
+				ExecuteCommand('e c')
 				FreezeEntityPosition(PlayerPedId(), false)
 				ESX.ShowNotification('Non disponibile attualmente', false, false, 140)
 			end
@@ -55,3 +59,15 @@ RegisterNetEvent('ak_dev:RapinaAtm',function(morte)
 	})
 end)
 
+RegisterNetEvent('ak_illegal:qTarget',function()
+	ESX.TriggerServerCallback('ak_dev:CheckItem', function(DispositivoHack)
+		if DispositivoHack >= 1 then
+			FreezeEntityPosition(PlayerPedId(), true)
+			ExecuteCommand('me Rapinando Atm')
+			ExecuteCommand("e mechanic")
+			TriggerEvent('ak_dev:RapinaAtm')
+		else
+			ESX.ShowNotification("Non hai gli strumenti giusti per rapinare questo ATM")
+		end
+	end)
+end)
